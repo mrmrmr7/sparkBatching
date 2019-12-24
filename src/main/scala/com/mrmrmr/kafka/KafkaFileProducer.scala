14a562da175ee20c547ad2179715cf01032adc54
Aliaksandr_Zaparozht <aliaksandr_zaparozhtsau@epam.com>
2019-12-24 17:30:47 +0300

    input optimization

--- src/main/scala/com/mrmrmr/kafka/KafkaFileProducer.scala
+++ src/main/scala/com/mrmrmr/kafka/KafkaFileProducer.scala
@@ -9,7 +9,7 @@ import scala.io.Source
 
 object KafkaFileProducer {
   def main(args: Array[String]): Unit = {
-    val filePath = "D:\\course\\spark\\untitled\\src\\main\\resources\\data_mapped_optimized.txt"
+    val filePath = "D:\\course\\spark\\sparkMaven\\src\\main\\resources\\data_mapped_optimized.txt"
     val bootstrapServer = "sandbox-hdp.hortonworks.com:6667"
     val topic = "test"
     val source = Source.fromFile(filePath)
@@ -28,7 +28,6 @@ object KafkaFileProducer {
 
     val producer = new KafkaProducer[String, String](properties)
 
-//    producer.send(record)
     for (data <- source.getLines()) {
       counter += 1
       if (counter % 10000 == 0){
@@ -36,7 +35,6 @@ object KafkaFileProducer {
       }
       val record = new ProducerRecord[String, String](topic, "0", data)
       producer.send(record)
-      //MyCustomKafkaProducer.produce(bootstrapServer, topic, "0", data)
     }
     producer.close()
 
